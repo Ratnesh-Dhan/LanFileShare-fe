6c43dded6f3995a75911d88215398f11d21b0658
Ratnesh-Dhan <ratneshdhan@gmail.com>
2026-07-09 18:28:51 +0530

    upload progress

--- lib/api.ts
+++ lib/api.ts
@@ -1,3 +1,5 @@
+import { ApiFile } from "@/types/alltypes";
+
 const BASE_URL = "http://172.19.4.34:5000";
 
 export async function uploadFile(file: File, targetDeviceId?: string) {
@@ -11,6 +13,26 @@ export async function uploadFile(file: File, targetDeviceId?: string) {
   });
   if (!res.ok) throw new Error("Upload failed");
   return res.json();
+}
+
+export function uploadFieWithProgress(
+  file: File,
+  onProgress: (precent: number) => void,
+  targetDeviceId?: string,
+): Promise<ApiFile> {
+  return new Promise((resolve, reject) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    if (targetDeviceId) formData.append("targetDeviceId", targetDeviceId);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", `${BASE_URL}/uploads`);
+
+    xhr.upload.onprogress = (e) => {
+      if (e.lengthComputable)
+        onProgress(Math.round((e.loaded / e.total) * 100));
+    };
+  });
 }
 
 export async function getFiles() {
